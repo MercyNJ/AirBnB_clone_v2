d88f60b1891fa739b084ea24ccf64de73ecd3b5f
MercyNJ <mercinjuguna@gmail.com>
2023-09-21 02:39:41 -0700

    Introduced 3rd route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""This script that starts a Flask web application"""
+"""A Script that starts a Flask web application """
+
 from flask import Flask
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """print web"""
-    return 'Hello HBNB!'
+    """print given message"""
+    return ('Hello HBNB!')
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """print web"""
-    return 'HBNB'
+    """Prints hbnb"""
+    return ('HBNB')
 
 
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """print C followed by the value of the text variable"""
-    return 'C {}'.format(text.replace('_', ' '))
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Displays C then value of text var"""
+    text = text.replace('_', ' ')
+    return ('C {}'.format(text))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
