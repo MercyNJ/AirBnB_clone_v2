7fdb8f53ae7e267ff0e0076c27a1cf8a12714396
MercyNJ <mercinjuguna@gmail.com>
2023-09-21 02:06:51 -0700

    Script starting a flask web app

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-""" This Script that starts a Flask web application """
+"""A Script that starts a Flask web application """
+
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Print Web """
-    return 'Hello HBNB!'
+    """print given message"""
+    return ('Hello HBNB!')
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Print Web """
-    return 'HBNB'
+    """Prints hbnb"""
+    return ('HBNB')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
