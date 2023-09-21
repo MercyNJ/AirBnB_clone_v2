18283c59f54c66b222f375df6f331c1eeeedcfbb
MercyNJ <mercinjuguna@gmail.com>
2023-09-21 07:26:31 -0700

    Corrections

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,16 +5,18 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
 /states_list: HTML page with a list of all State objects in DBStorage.
 """
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
 def close_db(exc):
-    """This close the current session of sqlalchemist"""
+    """This closes current session of sqlalchemy"""
     storage.close()
 
 
@@ -29,5 +31,4 @@ def states_list():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
