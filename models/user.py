63bc6fa8d2de77c765c46223bb9c3d11e59ccc99
MercyNJ <mercinjuguna@gmail.com>
2023-09-22 07:27:41 -0700

    corrections

--- models/user.py
+++ models/user.py
@@ -12,7 +12,7 @@ class User(BaseModel, Base):
     """This is the class reprensentation for user
     Attributes:
         email: email address
-        password: password for you login
+        password: password for login
         first_name: first name
         last_name: last name
     """
