7e0a4b1cf1e43c924b3cf3e298280946222f6258
MercyNJ <mercinjuguna@gmail.com>
2023-09-22 07:57:21 -0700

    correction

--- models/__init__.py
+++ models/__init__.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory
-and creates a unique FileStorage instance for your application"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from os import getenv
+"""A module instantiating an object of class FileStorage"""
+import os
 
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+type_storage = os.getenv('HBNB_TYPE_STORAGE')
+
+
+if type_storage == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
